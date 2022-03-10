2a7606b8b579b90fe093eb6dad3bf94819c7224b
AndyRobotsNC <andyrobotsdroid@gmail.com>
2022-03-10 11:45:21 +0000

    refactored sorting arrays

--- src/components/AllFilesAndFolders.js
+++ src/components/AllFilesAndFolders.js
@@ -2,57 +2,48 @@ import jsonData from "./data/files.json";
 import SingleItem from "./SingleItem";
 
 const AllFilesAndFolders = ({ sortBy }) => {
-  let nameSortedArr = [];
-  let sizeSortedArr = [];
-  let dateSortedArr = [];
+  //creating the name sorted array
+  let nameSortedArr = [...jsonData].sort((a, b) =>
+    a.name.localeCompare(b.name)
+  );
+  //creating the size sorted array
+  const singleFileArr = jsonData.filter((file) => {
+    return !("files" in file);
+  });
+  const foldersArr = jsonData.filter((file) => {
+    return "files" in file;
+  });
+  foldersArr.sort((a, b) => b.files.length - a.files.length);
+  let sizeSortedArr = foldersArr.concat(singleFileArr);
 
-  switch (sortBy) {
-    case "name":
-      nameSortedArr = [...jsonData].sort((a, b) =>
-        a.name.localeCompare(b.name)
-      );
-      break;
-    case "size":
-      const singleFileArr = jsonData.filter((file) => {
-        return !("files" in file);
+  //creating the date sorted array
+  let dateArr = jsonData.map((file) => {
+    if (file.type !== "folder") {
+      file.added = new Date(file.added);
+    } else {
+      file.files.forEach((item) => {
+        item.added = new Date(item.added);
       });
-      const foldersArr = jsonData.filter((file) => {
-        return "files" in file;
+    }
+    return file;
+  });
+  const singleDateFileArr = dateArr.filter((file) => {
+    return !("files" in file);
+  });
+  const dateFoldersArr = dateArr.filter((file) => {
+    return "files" in file;
+  });
+  singleDateFileArr.sort((a, b) => b.added - a.added);
+  let dateSortedArr = singleDateFileArr.concat(dateFoldersArr);
+  dateSortedArr.forEach((file) => {
+    if (file.type !== "folder") {
+      file.added = file.added.toString().slice(3, 15);
+    } else {
+      file.files.forEach((item) => {
+        item.added = item.added.toString().slice(3, 15);
       });
-      foldersArr.sort((a, b) => b.files.length - a.files.length);
-      sizeSortedArr = foldersArr.concat(singleFileArr);
-      break;
-    case "date":
-      let dateArr = jsonData.map((file) => {
-        if (file.type !== "folder") {
-          file.added = new Date(file.added);
-        } else {
-          file.files.forEach((item) => {
-            item.added = new Date(item.added);
-          });
-        }
-        return file;
-      });
-      const singleDateFileArr = dateArr.filter((file) => {
-        return !("files" in file);
-      });
-      const dateFoldersArr = dateArr.filter((file) => {
-        return "files" in file;
-      });
-      singleDateFileArr.sort((a, b) => b.added - a.added);
-      dateSortedArr = singleDateFileArr.concat(dateFoldersArr);
-      dateSortedArr.forEach((file) => {
-        if (file.type !== "folder") {
-          file.added = file.added.toString().slice(3, 15);
-        } else {
-          file.files.forEach((item) => {
-            item.added = item.added.toString().slice(3, 15);
-          });
-        }
-      });
-      break;
-    default:
-  }
+    }
+  });
 
   return (
     <div className="container">
